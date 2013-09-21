e6cca6a55d3c0d4fdddc952379971cca511920a9
Nemikolh <joan.varvenne@gmail.com>
2013-09-21 16:42:56 +0200

    gl_attribute_binder is now in its final version.

--- NKH/src/core/gl/glbinder.hpp
+++ NKH/src/core/gl/glbinder.hpp
@@ -42,10 +42,14 @@ struct gl_attribute_binder
         // ------------------------- DECLARE ------------------------ //
 
         attribute_id = glGetAttribLocation(&m_program_id, p_attribute_name);
+
+        // The attribute id can -1 if the attribute is not used inside the shader
+        // or if the attribute start with the reserved prefix "gl_"
         if(attribute_id != -1)
         {
             glVertexAttribPointer(attribute_id, p_nb_component, p_component_type, GL_FALSE, p_stride, p_offset);
             glEnableVertexAttribArray(attribute_id);
+            glVertexAttribDivisor(attribute_id, m_divisor);
         }
     }
 
@@ -53,11 +57,38 @@ struct gl_attribute_binder
     // =========================== CTOR/DTOR ========================== //
     // ================================================================ //
 
+    /**
+     * \brief Default constructor.
+     */
     gl_attribute_binder()
-        : m_program_id{0}
+        : m_divisor{0}
+        , m_program_id{0}
     {}
 
+    /**
+     * \brief Proper constructor.
+     * @param p_program_id is the id of the gl_program.
+     * @param p_divisor is the divisor for the attribute.
+     */
+    gl_attribute_binder(gl_types::id p_program_id, char p_divisor = 0)
+        : m_divisor{p_divisor}
+        , m_program_id{p_program_id}
+    {}
+
+    /**
+     * \brief Move constructor.
+     */
+    gl_attribute_binder(gl_attribute_binder&&) = default;
+
+    /**
+     * \brief Copy constructor.
+     */
+    gl_attribute_binder(const gl_attribute_binder&) = default;
+
+
 private:
+    /** The attribute divisor parameter. */
+    char            m_divisor;
     /** The program id currently bound. */
     gl_types::id    m_program_id;
 };

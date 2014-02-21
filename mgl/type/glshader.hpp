4913eaba755ad247eda16fc8f0b2a233086a4866
Nemikolh <joan.varvenne@gmail.com>
2014-02-21 10:53:20 +0100

    Added wrapper for shaders with unit test.

---
+++ mgl/type/glshader.hpp
@@ -0,0 +1,157 @@
+/*
+ * glshader.hpp
+ *
+ *  Created on: 20 févr. 2014
+ *      Author: nemikolh
+ */
+
+#ifndef GL_TYPE_SHADER_HPP_
+#define GL_TYPE_SHADER_HPP_
+
+#include <boost/noncopyable.hpp>
+#include "gltraits.hpp"
+#include "../glexceptions.hpp"
+
+namespace mgl {
+
+enum class shader_type
+{
+    VERTEX_SHADER,
+    TESS_CONTROL_SHADER,
+    TESS_EVALUATION_SHADER,
+    GEOMETRY_SHADER,
+    FRAGMENT_SHADER,
+    COMPUTE_SHADER
+};
+
+struct gl_shader
+{
+    // ================================================================ //
+    // =========================== CTOR/DTOR ========================== //
+    // ================================================================ //
+
+    gl_shader()
+        : m_shader_id(0)
+        , m_shader_type(shader_type::VERTEX_SHADER)
+    {}
+
+    gl_shader(shader_type p_type)
+        : m_shader_id(0)
+        , m_shader_type(p_type)
+    {}
+
+    /**
+     * @brief Copy isn't allowed.
+     */
+    gl_shader(const gl_shader&) = delete;
+    const gl_shader& operator=(const gl_shader&) = delete;
+
+    /**
+     * @brief Move constructor.
+     * @param p_rhs is the temporary moved here.
+     */
+    gl_shader(gl_shader&& p_rhs)
+        : m_shader_id(p_rhs.m_shader_id)
+        , m_shader_type(p_rhs.m_shader_type)
+    {
+        p_rhs.m_shader_id = 0;
+    }
+
+    const gl_shader& operator=(gl_shader&& p_rhs)
+    {
+        release();
+        m_shader_id = p_rhs.m_shader_id;
+        m_shader_type = p_rhs.m_shader_type;
+        p_rhs.m_shader_id = 0;
+        return *this;
+    }
+
+    ~gl_shader()
+    {
+        release();
+    }
+
+    // ================================================================ //
+    // ============================ METHODS =========================== //
+    // ================================================================ //
+
+    /**
+     * @brief Load the passed source to this shader.
+     * @param p_src is the source to load.
+     */
+    void load_src(const char* p_src)
+    {
+        static_assert(sizeof(char) == sizeof(GLchar), "This function will not work properly.");
+        // ------------------------- DECLARE ------------------------ //
+
+        ensure_created();
+        gl_object_shader::gl_shader_source(m_shader_id, 1, &p_src, nullptr);
+        gl_object_shader::gl_compile(m_shader_id);
+
+        if(!gl_object_shader::gl_compile_status(m_shader_id))
+            throw gl_compile_error(gl_object_shader::gl_info_log(m_shader_id));
+    }
+
+    /**
+     * @brief Load the passed source to this shader.
+     * @param p_src is the string source to load.
+     */
+    template<typename String>
+    void load_src(String p_src)
+    {
+        auto c_str = p_src.c_str();
+        // ------------------------- DECLARE ------------------------ //
+
+        load_src(c_str);
+    }
+
+private:
+    // ================================================================ //
+    // ============================ METHODS =========================== //
+    // ================================================================ //
+
+    void ensure_created()
+    {
+        if(!m_shader_id)
+            m_shader_id = gl_object_shader::gl_gen(convert_to_en(m_shader_type));
+    }
+
+    void release()
+    {
+        if(m_shader_id)
+            gl_object_shader::gl_delete(1, &m_shader_id);
+    }
+
+    GLenum convert_to_en(shader_type p_tag)
+    {
+        switch (p_tag) {
+            case shader_type::VERTEX_SHADER:
+                return GL_VERTEX_SHADER;
+            case shader_type::TESS_CONTROL_SHADER:
+                return GL_TESS_CONTROL_SHADER;
+            case shader_type::TESS_EVALUATION_SHADER:
+                return GL_TESS_EVALUATION_SHADER;
+            case shader_type::GEOMETRY_SHADER:
+                return GL_GEOMETRY_SHADER;
+            case shader_type::FRAGMENT_SHADER:
+                return GL_FRAGMENT_SHADER;
+            case shader_type::COMPUTE_SHADER:
+                return GL_COMPUTE_SHADER;
+        }
+    }
+
+
+    // ================================================================ //
+    // ============================= FIELDS =========================== //
+    // ================================================================ //
+
+    /** The id of the shader. */
+    gl_types::id m_shader_id;
+    /** The type of the shader. */
+    shader_type m_shader_type;
+};
+
+}  /* namespace mgl */
+
+
+#endif /* GL_TYPE_SHADER_HPP_ */
---
+++ test/ShaderProperUse.h
@@ -0,0 +1,56 @@
+#ifndef SHADERPROPERUSE_H_
+#define SHADERPROPERUSE_H_
+
+#include <cxxtest/TestSuite.h>
+#include <SFML/Graphics.hpp>
+
+#include "../mgl/glvector.hpp"
+#include "../mgl/glrequires.hpp"
+#include "../mgl/type/glshader.hpp"
+
+using namespace mgl;
+
+class ShaderProperUse : public CxxTest::TestSuite
+{
+    std::unique_ptr<sf::Window> window;
+public:
+    void setUp()
+    {
+        sf::ContextSettings settings;
+        settings.majorVersion = 3;
+        settings.minorVersion = 3;
+
+        window.reset(new sf::Window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings));
+        window->setVisible(false);
+
+        GLenum err = glewInit();
+        if (GLEW_OK != err)
+        {
+            std::cerr << "GLEW error: " << glewGetErrorString(err) << std::endl;
+        }
+
+        if (!glewIsSupported("GL_VERSION_3_0"))
+        {
+            std::cerr << "OpenGL version 3.0 isn't supported." << std::endl;
+        }
+    }
+
+    void tearDown()
+    {
+        window->close();
+        window.reset();
+    }
+
+    void testCompileShader()
+    {
+        gl_shader shader(shader_type::VERTEX_SHADER);
+        TS_ASSERT_THROWS(shader.load_src("abc"), gl_compile_error&);
+        TS_ASSERT_THROWS_NOTHING(priv::glTryError());
+
+        TS_ASSERT_THROWS_NOTHING(shader.load_src("#version 330\nin vec4 vertex;\nvoid main(void){gl_Position = vertex;}"));
+        TS_ASSERT_THROWS_NOTHING(priv::glTryError());
+    }
+
+};
+
+#endif /*SHADERPROPERUSE_H_*/

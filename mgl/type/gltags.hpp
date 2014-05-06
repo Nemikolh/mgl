1ddb76dee3bb6e1d3870694af3e8a6f071653f6a
Nemikolh <joan.varvenne@gmail.com>
2014-05-06 19:36:11 +0100

    Added Tag for the gl_Position.

---
+++ mgl/type/gltags.hpp
@@ -0,0 +1,22 @@
+/*
+ * gltags.hpp
+ *
+ *  Created on: 6 mai 2014
+ *      Author: nemikolh
+ */
+
+#ifndef GLTAGS_HPP_
+#define GLTAGS_HPP_
+
+namespace mgl {
+
+/**
+ * Tag to assign an attribute to the gl_Position attribute.
+ */
+struct gl_tag_position {};
+
+}  /* namespace mgl */
+
+
+
+#endif /* GLTAGS_HPP_ */
